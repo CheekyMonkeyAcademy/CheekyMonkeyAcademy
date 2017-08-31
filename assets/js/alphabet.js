cb8d0f3cfa5aae3f059d7a24c280d1c7a07864f2
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-08-31 13:00:55 -0600

    pushed the needle forward on alphabet - giphy search is not looking promising - LOTS of bad data -  need a solution there

--- assets/js/alphabet.js
+++ assets/js/alphabet.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 			alphabetGame: {
 				correctLetter: "A",
 				fourRandomLettersArray: [],
-				alphabetArray: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+				alphabetArray: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+				gifMovement: true
 			},
 			otherGame: {
 
@@ -47,10 +48,15 @@ $(document).ready(function(){
 		for (i = 0; i < gameObject.alphabetGame.fourRandomLettersArray.length; i++) {
 			var thisDiv = "div" + (i);
 			var thisLetter = gameObject.alphabetGame.fourRandomLettersArray[i]
-			var thisSearchTerm = "The Letter " + thisLetter;
-			console.log("Div: " + thisDiv + " assigned letter: " + thisLetter);
+			var thisSearchTerm = "alphabet letter " + thisLetter;
+			// TODO a search term for letters seems not be the best way to get gifs - we might need to go from a static gif set
+			// Or find a search term that works more unformly (I doubt one exists)
+			// alternatively, we just accept the random fails and move forward?
+			// perhaps rotate through gifs every few seconds?  give the page more of a 'live' feel?   
+			// dunno, this is a bit of a work in progress.  
+			console.log("Div: " + thisDiv + " assigned letter: " + thisLetter + " using search: >" + thisSearchTerm + "<");
 
-			getGifsAndAssignToDiv(thisSearchTerm, thisDiv);
+			getGifAndAssignToDiv(thisSearchTerm, thisDiv);
 
 		}
 	}
@@ -75,10 +81,10 @@ $(document).ready(function(){
 		window.speechSynthesis.speak(msg);
 	}	
 
-	function getGifsAndAssignToDiv(searchTerm, div) {
+	function getGifAndAssignToDiv(searchTerm, div) {
 		var resultsObject = {};
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    	searchTerm + "&api_key=dc6zaTOxFJmzC&limit=1";
+    	searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10"; // This should end up using 1 - maybe pull more and rotate based on a timer?
 
     	$.ajax({
         	url: queryURL,
@@ -92,20 +98,22 @@ $(document).ready(function(){
 	        var stillUrl = value.images.fixed_height_still.url;
 	        var rating = value.rating;
 
-	        // var thisP = $("<p>");
-	        // thisP.text("Rating: " + rating.toUpperCase());
-	        var targetParent = $("#" + div);
-	        var thisDiv = $("<div>");
-	        var thisGif = $("<img>");
-	        thisDiv.attr("class", "div-css")
-	        thisGif.attr("src", movingUrl);
-	        thisGif.attr("still_url", stillUrl);
-	        thisGif.attr("moving_url", movingUrl);
-	        thisGif.attr("current_state", "moving")
-	        thisGif.attr("class", "gif");
-	        // thisDiv.prepend(thisP);
-	        thisDiv.append(thisGif);
-	        targetParent.prepend(thisDiv);
+			if (rating = "G") { // kids game, let's keep it clean - can variable this later.  
+		        var thisP = $("<p>");
+		        thisP.text("Rating: " + rating.toUpperCase());
+		        var targetParent = $("#" + div);
+		        var thisDiv = $("<div>");
+		        var thisGif = $("<img>");
+		        thisDiv.attr("class", "div-css")
+		        thisGif.attr("src", movingUrl);
+		        thisGif.attr("still_url", stillUrl);
+		        thisGif.attr("moving_url", movingUrl);
+		        thisGif.attr("current_state", "moving")
+		        thisGif.attr("class", "gif");
+		        thisDiv.prepend(thisP);
+		        thisDiv.append(thisGif);
+		        targetParent.prepend(thisDiv);
+	    	}
       	});
 
 		// TODO insure that we are in compliance with the rating requested (more gifs required here);
