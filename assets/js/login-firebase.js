3cea424465514f27bcb92e6435a0639d6a7b1b06
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-09-05 13:09:02 -0600

    initial stab at adding login via firebase - WIP

---
+++ assets/js/login-firebase.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDfepSNI19ZoWaOPv52mXSUN4s_W8O8URg",
+    authDomain: "cheekymonkeyacademy-1a390.firebaseapp.com",
+    databaseURL: "https://cheekymonkeyacademy-1a390.firebaseio.com",
+    projectId: "cheekymonkeyacademy-1a390",
+    storageBucket: "",
+    messagingSenderId: "52825639428"
+  };
+  firebase.initializeApp(config);
+
+});
