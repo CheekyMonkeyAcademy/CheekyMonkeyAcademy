32664270b22ced9a64d9a86d6fb7ba84705c43bc
KOM2005 <oleh_kmyta@comcast.net>
2017-09-05 15:04:04 -0600

    some changes

---
+++ assets/js/animals.js
@@ -0,0 +1,156 @@
+$(document).ready(function(){
+	var gameObject = {
+			divs: ["div0", "div1", "div2", "div3"],
+			timer: {
+
+			},
+			animalGame: {
+				correctAnimal: "Ants",
+				fourRandomAnimalsArray: [],
+				animalArray: ["Ants", "Bat", "Cat", "Dog", "Eagle", "Fish",
+				 "Goose", "Hamster", "Impala", "Jaguar", "Kangaroo", "Lion",
+				  "Monkey", "Newfoundland", "Ostrich", "Pig", "Quinling panda", "Raccoon",
+				   "Salamander", "Tiger", "Urchin", "Vulture", "Wolf", "Xerus squirrel", 
+				   "Yellow mongoose", "Zebra"],
+				gifMovement: true
+			},
+			otherGame: {
+
+			},
+			yetAnotherGame: {
+
+			}
+	};
+	
+
+
+
+	// console.log(gameObject)
+	// gameObject.divs.forEach(function(div) {
+	// 	console.log(div);
+	// });
+
+
+	var animalToClick = "Click on the animal" + gameObject.animalGame.correctAnimal;
+
+	// computerSayThis(animalToClick);
+	// computerSayThis("That was AWESOME!!!");
+
+	// TODO load 4 items from the animal array - load 1 into each div as the target image
+
+	
+	// TODO click the right and get a good sound and a new animal
+
+	// TODO click the wrong one and get a bad sound - no new animal
+
+	assignAnimalAndCallForGifToDiv();
+
+	function assignAnimalAndCallForGifToDiv() {
+		clearDivs();
+		getFourRandomAnimalsIntoArray();
+		// we have 4 divs and 4 animals - select one of each and assign
+
+		for (i = 0; i < gameObject.animalGame.fourRandomAnimalsArray.length; i++) {
+			var thisDiv = "div" + (i);
+			var thisAnimal = gameObject.animalGame.fourRandomAnimalsArray[i];
+			var thisSearchTerm = "animal " + thisAnimal;
+			// TODO a search term for animals seems not be the best way to get gifs - we might need to go from a static gif set
+			// Or find a search term that works more unformly (I doubt one exists)
+			// alternatively, we just accept the random fails and move forward?
+			// perhaps rotate through gifs every few seconds?  give the page more of a 'live' feel?   
+			// dunno, this is a bit of a work in progress.  
+			console.log("Div: " + thisDiv + " assigned animal: " + thisAnimal + " using search: >" + thisSearchTerm + "<");
+
+
+			getGifAndAssignToDiv(thisSearchTerm, thisDiv);
+		}
+		// assign our 'winning' animal for this round.
+		gameObject.animalGame.correctAnimal = gameObject.animalGame.fourRandomAnimalsArray[getRandomFrom(4)];
+		// voice active asking kids to find said animal
+		computerSayThis("find " + gameObject.animalGame.correctAnimal);
+	}
+
+	function clearDivs() {
+		gameObject.divs.forEach(function(div) {
+			$("#"+div).empty();
+			console.log("emptying: " + div);
+		});
+	}
+
+	function getFourRandomAnimalsIntoArray() {
+		gameObject.animalGame.fourRandomAnimalsArray = [];
+		for (i = 0; i < 4; i++) {
+			gameObject.animalGame.fourRandomAnimalsArray.push(gameObject.animalGame.animalArray[getRandomFrom(26)]);
+			// TODO this can duplicate animals - do we want to eliminate that?  Or is a duplicate animal such a good thing?
+			// If we have randomized gifs inside of a animal I would almost leave the 'mulitple success' in.  Sometimes mulitple things ARE true in life.
+		}
+		console.log(gameObject.animalGame.fourRandomAnimalsArray);
+	}
+		
+	function getRandomFrom(thisNumber) {
+		var returnValue = Math.floor(Math.random() * thisNumber);
+		return returnValue;
+	}
+
+	function computerSayThis(thingToSay) {
+		var msg = new SpeechSynthesisUtterance(thingToSay);
+		// TODO check for other voices
+		window.speechSynthesis.speak(msg);
+	}	
+
+	function getGifAndAssignToDiv(searchTerm, div) {
+		var resultsObject = {};
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "funny " +
+    	searchTerm + "&api_key=dc6zaTOxFJmzC&limit=1"; // This should end up using 1 - maybe pull more and rotate based on a timer?
+
+    	$.ajax({
+        	url: queryURL,
+        	method: "GET"
+        }).done(function(response) {
+
+	      	resultsObject = response.data;
+	      	// TODO grab one from a randomized set - using 10 for testing (so I can see how the search terms look... it's not good)
+	      	$.each(resultsObject, function(key, value) {
+		        var movingUrl = value.images.fixed_height.url;
+		        var stillUrl = value.images.fixed_height_still.url;
+		        var rating = value.rating;
+
+				if (rating = "r") { // kids game, let's keep it clean - can variable this later.  
+			        var thisP = $("<p>");
+			        thisP.text("Rating: " + rating.toUpperCase());
+			        var targetParent = $("#" + div);
+			        var thisDiv = $("<div>");
+			        var thisGif = $("<img>");
+			        // thisDiv.attr("class", "div-css")
+			        thisGif.attr("src", movingUrl);
+			        thisGif.attr("still_url", stillUrl);
+			        thisGif.attr("moving_url", movingUrl);
+			        thisGif.attr("current_state", "moving");
+			        thisGif.attr("assigned_animal", searchTerm);
+			        thisGif.attr("class", "gif");
+			        thisDiv.prepend(thisP);
+			        thisDiv.append(thisGif);
+			        targetParent.prepend(thisDiv);
+		    	}
+	      	});
+
+		// TODO insure that we are in compliance with the rating requested (more gifs required here);
+		});
+
+	}
+
+	$("#clicky-container").on("click", ".gif", function(){
+		console.log("received click on: " + this);
+		console.log(this);
+		console.log($(this).attr("assigned_animal"));
+		console.log("animal " + gameObject.animalGame.correctAnimal);
+		if ($(this).attr("assigned_animal") === ("animal " + gameObject.animalGame.correctAnimal)) {
+			computerSayThis("This is correct!!!");
+			 // + gameObject.animalGame.correctAnimal);
+			assignAnimalAndCallForGifToDiv();
+		}
+		else {
+			// TODO play a fun sound that is a 'nope'
+		}
+	});
+});
