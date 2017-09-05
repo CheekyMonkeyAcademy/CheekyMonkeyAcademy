0c114cedf46c87b07abfa3f53c99d883d42f54f7
Robert Rollner <robrollner@gmail.com>
2017-09-05 14:25:48 -0600

    updating shapes js logic, attemtping to use getty image API for shape pulling

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,4 +1,5 @@
-//ApplicationRobert Rollner Key: e24ad6a7dkbrsux488zarzzv 
+//ApplicationRobert Rollner 
+//Key: e24ad6a7dkbrsux488zarzzv 
 //Secret: DPNf2zP9ad6bmw7XSzSepasNwKZ3CKCn7VmmyBdk2sZRd 
 //Status: active Created: 30 seconds ago
 //Key Rate Limits
@@ -32,7 +33,56 @@ $(document).ready(function() {
     		var thisDiv = "div" + (i);
     		var thisShape = gameObject.shapesGame.fourRandomShapesArray[i];
     		var thisSearchTerm = "shape " + thisShape;
+
+    		console.log("Div: " + thisDiv + " assigned shape " + thisShape + " using search: >" + thisSearchTerm + "<");
+
+    		getShapeAndAssignToDiv(thisSearchTerm, thisDiv);
     	}
+    	//assigning winning shape
+    	gameObject.shapesGame.correctShape = gameObject.shapesGame.fourRandomShapesArray[getRandomFrom(4)];
+    	//voice prompt for shape
+    	computerSayThis("Please click on the image for the shape " + gameObject.shapesGame.correctShape);
+
     }
 
+    function clearDivs() {
+    	gameObject.divs.forEach(function(div) {
+    		$("#" + div).empty();
+    		console.log("emptying: " + div);
+    	});
+    }
+
+    function getFourRandomShapesIntoArray() {
+    	gameObject.shapesGame.fourRandomShapesArray = [];
+    	for (i = 0; i < 4; i++) {
+    		gameObject.shapesGame.fourRandomShapesArray.push(gameObject.shapesGame.shapesArray[getRandomFrom(9)]);
+    	}
+    	console.log(gameObject.shapesGame.fourRandomShapesArray);
+    }
+
+    function getRandomFrom(thisNumber) {
+    	var returnValue = Math.floor(Math.random() * thisNumber);
+    	return returnValue;
+    }
+
+    function computerSayThis(thingsToSay) {
+    	var msg = new SpeechSynthesisUtterance(thingsToSay);
+    	window.speechSynthesis.speak(msg);
+    }
+
+    function getShapeAndAssignToDiv(searchTerm, div) {
+    	var resultsObject = {},
+    	var queryURL = "https://api.gettyimages.com/v3/search/images/creative?phrase=" + searchTerm + "&api_key=e24ad6a7dkbrsux488zarzzv"
+
+    	$.ajax({
+    		url: queryURL,
+    		method: "GET"
+    	}).done(function(response) {
+    		resultsObject = response.data;
+
+    		$.each(resultsObject, function(key, value) {
+    			var ImageURL = value.
+    		})
+    	})
+    }
 }); //end doc.ready function
