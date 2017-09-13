b5b16d14292607b7f75b03a3c4c822af24a1268a
neliza <nicolasaelf64@gmail.com>
2017-09-13 08:52:13 -0600

    list of sports

---
+++ assets/js/matchingGame.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+//With JS variables, create option for user to
+	//create a random number of cards
+	//available on screen
+
+
+
+//Create an array of sports available
+
+var gameObject = {
+	sportsArray: ["baseball", "soccer", "american football", "tennis", 
+	"golf", "volleyball", "yoga", "gymnastics", "boxing", "hockey", "hiking", "bowling", 
+	"softball", "track", "surfing", "martial arts", "figure skating", "billiards", "swimming"]
+}
+
+//Set up API call to getty images
+
+//Depending on user choice,
+	//populate the number of cards selected
+
+//Create a function:
+	//If the cards match they stay turned over
+	//If the cards to not match they turn over
+
+
+
+}) //End of the document.ready function
