b122146f4cebef79b3cae996a09119bc6e8d6b01
neliza <nicolasaelf64@gmail.com>
2017-09-14 10:29:18 -0600

    API call working

--- assets/js/matchingGame.js
+++ assets/js/matchingGame.js
@@ -11,35 +11,72 @@ $(document).ready(function(){
 	}
 }
 
+// numberOfCardDivs = [];
+
 console.log("#ofSports: " + gameObject.matchGame.numberOfSports); //THIS WORKS!!
 console.log("#ofCards: " + gameObject.matchGame.numberOfCards); //THIS WORKS!!
 
-function storeAndPopulateAmountOfCards (){
+function storeAndPopulateAmountOfCards() {
 
 	$("#makeCards").on("click", function(){
-	var numberOfSports = $("#numberOfSportsChosen").val();
-	var cardsPerSport = $("#cardsPerSportChosen").val();
-	var cardDisplay = numberOfSports * cardsPerSport;
-	
-	console.log("how many cards: " + cardDisplay);
+	var numberOfSports = $("#numberOfSportsChosen").val(); //THIS WORKS!!
+	var cardsPerSport = $("#cardsPerSportChosen").val(); //THIS WORKS!!
+	// var cardDisplay = numberOfSports * cardsPerSport;
+
+	//THIS WORKS!!!
+	for (var i = 0; i <  (numberOfSports * cardsPerSport); i++) {
+		var newCardDiv = $("<div>");
+		newCardDiv.addClass("newCardStyle");
+		$("#cardContainer").append(newCardDiv);
+		console.log(newCardDiv);
+		}
 	})
 }
 
 storeAndPopulateAmountOfCards();
 
+//Reset the board for player to play again 
+function resetTheBoard (){
+	$("#resetBoard").on("click", function(){
+		$("#cardContainer").html("");
+		$("#numberOfSportsChosen").val("");
+		$("#cardsPerSportChosen").val("");
+	})
+}
+
+resetTheBoard();
+
+//API FOR IMAGES CODE!!!!!
+function assignSportImageToCard(searchSport){
+var apiKey = "y482smscwdrv44u8raqcw77t";
+var appendApiKeyHeader = function( xhr ) {
+  xhr.setRequestHeader('Api-Key', apiKey)
+};
+
+
+$.ajax({
+	type: "GET",
+	beforeSend:appendApiKeyHeader,
+	url: "https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=" + searchSport
+})
+	.done(function(responseData){
+		console.log(responseData);
+		console.log(responseData.images[0].display_sizes[0].uri);
+	})
+} 
+
+
+assignSportImageToCard("baseball"); 
 
 
 
-
-//Set up API call to getty images
-
-//Depending on user choice,
-	//populate the number of cards selected
-
-//Create a function:
-	//If the cards match they stay turned over
-	//If the cards to not match they turn over
+}) //End of the document.ready function 
 
 
+//Before you make cards, we will get our sports from the array
 
-}) //End of the document.ready function
+//when we make the cards, we need to make number of cards
+	//We need a class before we make cards
+	//assign class that will allow us to assign an image
+	//number of cards that have a class of sport #
+	//Can't create divs first
