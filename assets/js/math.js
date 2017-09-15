eecd6250a2ca4e4db28dd0f9fd46851cc8b5d659
KOM2005 <oleh_kmyta@comcast.net>
2017-09-14 19:06:57 -0600

    added radio buttons and added functions to math.js

--- assets/js/math.js
+++ assets/js/math.js
@@ -8,11 +8,14 @@ $(document).ready(function() {
     function assignAnswerAndCallForGifToDiv() {
         clearDivs();
         storeAndPopulateNumberVariables();
-        getFourRandomAnswersIntoArray();
+         getFourRandomAnswersIntoArray("division");
+        getFourRandomAnswersIntoArray("substraction");
+        getFourRandomAnswersIntoArray("multiplication");
+        getFourRandomAnswersIntoArray("addition");
 
         for (i = 0; i < gameObject.mathGame.fourRandomAnswersArray.length; i++) {
             var thisDiv = "div" + (i);
-            var thisAnswer = gameObject.mathGame.fourRandomAnswersArray[i][1]
+            var thisAnswer = gameObject.mathGame.fourRandomAnswersArray[i][1];
             var randomAnimal = gameObject.animalGame.animalArray[getRandomFrom(gameObject.animalGame.animalArray.length)];
             var thisSearchTerm = "nature " + randomAnimal;
 
@@ -26,17 +29,17 @@ $(document).ready(function() {
             thisParentDiv.prepend(newFloatingDiv);
         }
         // assign our 'winning' letter for this round
-        var thisRandom = getRandomFrom(4)
-        console.log(gameObject.mathGame)
+        var thisRandom = getRandomFrom(4);
+        console.log(gameObject.mathGame);
 
         gameObject.mathGame.correctAnswer = gameObject.mathGame.fourRandomAnswersArray[thisRandom][1].toString();
-        gameObject.mathGame.correctQuestion = gameObject.mathGame.fourRandomAnswersArray[thisRandom][0]
+        gameObject.mathGame.correctQuestion = gameObject.mathGame.fourRandomAnswersArray[thisRandom][0];
         // voice active asking kids to find said letter
         computerSayThis("Please click the answer to " + gameObject.mathGame.correctQuestion);
-        $("#questionGoesHere").html("Solve this: <br>" + gameObject.mathGame.correctQuestion)
+        $("#questionGoesHere").html("Solve this: <br>" + gameObject.mathGame.correctQuestion);
     }
 
-    function getFourRandomAnswersIntoArray() {
+    function getFourRandomAnswersIntoArray(mathType) {
         gameObject.mathGame.fourRandomAnswersArray = [];
         for (i = 0; i < 4; i++) {
             var thisAnswer = 0;
@@ -53,18 +56,31 @@ $(document).ready(function() {
 
             // addition
             for (num = 0; num < numberOfNumbers; num++) {
-                var addMe = getRandomFromMinMax(minNumber, maxNumber)
-                thisAnswer += addMe;
+                var addMe = getRandomFromMinMax(minNumber, maxNumber);
                 if (thisQuestion === "") {
                     thisQuestion = "(" + addMe.toString();
+                    thisAnswer = addMe;
                 }
-                else {
+                else if (mathType === "addition") {
                     thisQuestion = thisQuestion + " + " + addMe.toString();
+                    thisAnswer += addMe;
+                }
+                  else if (mathType === "multiplication") {
+                    thisQuestion = thisQuestion + " * " + addMe.toString();
+                    thisAnswer = thisAnswer * addMe;
+                }
+                  else if (mathType === "substraction") {
+                    thisQuestion = thisQuestion + " - " + addMe.toString();
+                    thisAnswer -= addMe;
+                }
+                  else if (mathType === "division") {
+                    thisQuestion = thisQuestion + " / " + addMe.toString();
+                    thisAnswer = thisAnswer / addMe;
                 }
             }
             thisQuestion = thisQuestion + ")";
 
-            questionAnswerArray = [thisQuestion, thisAnswer]
+            questionAnswerArray = [thisQuestion, thisAnswer];
             gameObject.mathGame.fourRandomAnswersArray.push(questionAnswerArray);
             // end addition (others not implemented yet)
         }
@@ -75,25 +91,25 @@ $(document).ready(function() {
         var maxNumber = $("#maxNumber").val();
         var numberOfNumbers = $("#numberOfNumbers").val();
 
-        console.log(gameObject.mathGame)
+        console.log(gameObject.mathGame);
         if (minNumber == "") {
-            console.log("min empty")
-            $("#minNumber").val(gameObject.mathGame.minNumber)
+            console.log("min empty");
+            $("#minNumber").val(gameObject.mathGame.minNumber);
         }
         else {
-            console.log("min else")
+            console.log("min else");
             gameObject.mathGame.minNumber = parseInt($("#minNumber").val());
         }
 
         if (maxNumber == "") {
-            $("#maxNumber").val(parseInt(gameObject.mathGame.maxNumber))
+            $("#maxNumber").val(parseInt(gameObject.mathGame.maxNumber));
         }
         else {
             gameObject.mathGame.maxNumber = parseInt($("#maxNumber").val());
         }
 
         if (numberOfNumbers == "") {
-            $("#numberOfNumbers").val(gameObject.mathGame.numberOfNumbers)
+            $("#numberOfNumbers").val(gameObject.mathGame.numberOfNumbers);
         }
         else {
             gameObject.mathGame.numberOfNumbers = parseInt($("#numberOfNumbers").val());
@@ -102,7 +118,7 @@ $(document).ready(function() {
     }
 
     $("#clicky-container").on("click", ".clickable", function() {
-        console.log(gameObject.mathGame)
+        console.log(gameObject.mathGame);
         if ($(this).attr("assigned_thing") === (gameObject.mathGame.correctAnswer)) {
             getMessageForComputerToSay("success");
             computerSayThis("You clicked the " + gameObject.mathGame.correctAnswer);
@@ -110,5 +126,23 @@ $(document).ready(function() {
         } else {
             getMessageForComputerToSay("failure");
         }
+$(".radio").click(function() {
+
+
+    if (mathType === "addition") {
+     getFourRandomAnswersIntoArray("addition");
+    } else if (mathType === "multiplication") {
+      getFourRandomAnswersIntoArray("multiplication");
+    } else if (mathType === "substraction") {
+        getFourRandomAnswersIntoArray("substraction");
+    } else if (mathType === "division") {
+    getFourRandomAnswersIntoArray("division");
+}
+      
+ 
+   });
+  
+
     });
-});
+
+       });   
